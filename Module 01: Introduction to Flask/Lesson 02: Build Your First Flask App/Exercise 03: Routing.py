dc38fd1c59a3ee2578efc6b21ae2e43cd6198bdf
josejpalacios <jose.palacios6119@gmail.com>
2021-01-08 22:48:08 -0600

    Update Exercise 03: Routing.py

--- Module 01: Introduction to Flask/Lesson 02: Build Your First Flask App/Exercise 03: Routing.py
+++ Module 01: Introduction to Flask/Lesson 02: Build Your First Flask App/Exercise 03: Routing.py
@@ -1,1 +1,22 @@
+# Routing: Requests from different URLs directed to different endpoints
 
+# view function: contains the code for processing the request and generating a response
+
+# route(): bind a URL to the view function such that the function will be triggered when the URL is visited
+
+# Multiple URLs can also be bound to the same view function:
+from flask import Flask
+
+app = Flask(__name__)
+
+# Create home() function. Bind to URL path using route()
+@app.route('/')
+# Bind another URL path to home()
+@app.route('/home')
+def home():
+  return 'Hello, World!'
+
+# Create reporter() function. Bind to URL path using /reporter
+@app.route('/reporter')
+def reporter():
+  return 'Reporter Bio'
