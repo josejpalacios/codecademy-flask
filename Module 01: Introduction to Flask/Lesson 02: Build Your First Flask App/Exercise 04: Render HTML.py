8da4de7eb8ff11d6e15ffc74071267cef2665103
josejpalacios <jose.palacios6119@gmail.com>
2021-01-08 22:48:33 -0600

    Update Exercise 04: Render HTML.py

--- Module 01: Introduction to Flask/Lesson 02: Build Your First Flask App/Exercise 04: Render HTML.py
+++ Module 01: Introduction to Flask/Lesson 02: Build Your First Flask App/Exercise 04: Render HTML.py
@@ -1,1 +1,21 @@
+# Response from view function can also return HTML to be rendered on a webpage
+from flask import Flask
 
+# Can also use triple quotes to contain multi-line code
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    # Add <h1> tag to return value
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/reporter')
+def reporter():
+    # Add <h2> tag to return value. Add <a> tag to return value
+    return """
+    <h2>Reporter Bio</h2>
+    <a href='/'>Return to home page</a>
+    """
