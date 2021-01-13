0fffce92a83699d9923b99b2426f54f2529259cb
josejpalacios <jose.palacios6119@gmail.com>
2021-01-12 22:47:51 -0600

    Create Exercise 02: Queries: query-all() and query-get().py

---
+++ Module 03: Databases in Flask/Lesson 02: Databases in Flask - Reading, Updating and Deleting/Exercise 02: Queries: query-all() and query-get().py
@@ -0,0 +1,38 @@
+# TableName.query.all(): Get all entries from a model called TableName
+# TableName.query.get(ID): Get entry with a primary key value of ID from model TableName
+
+# Store result of .get() to a variable to get access to attributes of result.
+
+from app import db, Book, Reader, Review, Annotation
+
+#query all the readers from the Reader model
+readers = Reader.query.all()
+print(readers)
+
+#get an entry with id = 123 
+reader = Reader.query.get(123)
+print(reader)
+
+#reader with id = 450
+reader = Reader.query.get(450)
+print("Reader with id = ", reader.id, "is called", reader.name)
+
+#Loop through all the readers and print their e-mails
+print("\nPrint all the readers in a loop:")
+for reader in readers:
+  print(reader.email)
+
+#or inline
+#[print(reader.email) for reader in readers]
+
+print("\nCheckpoint1: fetching all the reviews")
+reviews = Review.query.all()
+
+print("\nCheckpoint2: looping through all the reviews and printing their text")
+#your loop line 1
+#you loop line 2
+for review in reviews:
+  print(review.text)
+
+print("\nCheckpoint3: fetching a book with id = 13 using the get() function")
+book_1 = Book.query.get(12)
