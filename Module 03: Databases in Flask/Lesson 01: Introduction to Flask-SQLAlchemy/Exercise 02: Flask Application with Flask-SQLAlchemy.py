4d22e2460202a1874563e1021bae0b14b0426107
josejpalacios <jose.palacios6119@gmail.com>
2021-01-12 22:18:29 -0600

    Create Exercise 02: Flask Application with Flask-SQLAlchemy.py

---
+++ Module 03: Databases in Flask/Lesson 01: Introduction to Flask-SQLAlchemy/Exercise 02: Flask Application with Flask-SQLAlchemy.py
@@ -0,0 +1,20 @@
+# Flask-SQLAlchemy is an extension for Flask that supports the use of a Python SQL Toolkit called SQLAlchemy.
+
+# To use, need to import SQLAlchemy class from flask_sqlalchemy module:
+# from flask_sqlalchemy import SQLAlchemy
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#database configuration
+app = Flask(__name__) #application instance
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db' #path to database and its name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warning
+db = SQLAlchemy(app) #database instance
+
+
+#some routing for displaying the home page
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Congrats! You have just created your first Flask application supporting databases!"
