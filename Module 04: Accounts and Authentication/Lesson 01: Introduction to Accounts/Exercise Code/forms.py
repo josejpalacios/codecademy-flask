3740a2d3f427fd22a3516fbb86e20d74b66bb927
josejpalacios <jose.palacios6119@gmail.com>
2021-01-16 17:43:36 -0600

    Update forms.py

--- Module 04: Accounts and Authentication/Lesson 01: Introduction to Accounts/Exercise Code/forms.py
+++ Module 04: Accounts and Authentication/Lesson 01: Introduction to Accounts/Exercise Code/forms.py
@@ -1,16 +1,33 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from models import User, DinnerParty
 
-# instantiate application and database
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# registration form
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Register')
 
-# create login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login form
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+                      validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember = BooleanField('Remember Me')
+  submit = SubmitField('Login')
 
-import routes, models
+# dinner party form
+class DinnerPartyForm(FlaskForm):
+  date = StringField('Date', validators=[DataRequired()])
+  venue = StringField('Venue', validators=[DataRequired()])
+  main_dish = StringField('Dish', validators=[DataRequired()])
+  number_seats = StringField('Number of Seats')
+  submit = SubmitField('Create')
+
+# rsvp form
+class RsvpForm(FlaskForm):
+  party_id = StringField('Party ID', validators=[DataRequired()])
+  submit = SubmitField('RSVP')
